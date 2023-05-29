65143519e4054c2ffab7dde8b69285e2ee8f9f7b
sunnyghosh108 <sunnyghoshindra009@gmail.com>
2023-05-30 04:53:18 +0600

    add to cart

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,8 @@ import Login from "../Pages/Home/Login/Login";
 import SignUp from "../Pages/Home/SignUp/SignUp";
 import Secret from "../Shared/Secret/Secret";
 import PrivateRoute from "./PrivateRoute";
+import Dashboard from "../Layout/Dashboard";
+import MyCart from "../Pages/DashBoard/MyCart";
 
 
    export const router = createBrowserRouter([
@@ -43,5 +45,16 @@ import PrivateRoute from "./PrivateRoute";
           element:<PrivateRoute><Secret></Secret></PrivateRoute>
         }
       ]
+     
     },
+    {
+      path:'dashboard',
+      element:<Dashboard></Dashboard>,
+      children:[
+        {
+          path:'mycart',
+          element:<MyCart></MyCart>
+        }
+      ]
+    }
   ]);
